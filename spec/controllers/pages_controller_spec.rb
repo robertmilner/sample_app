a091eb3b804bdf3c3d119137aeb439a0200f8fae
Robert Milner <dev@rbrtmlnr.com>
2012-01-29 23:43:50 -0600

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
-  describe "GET 'home'" do
-    it "should be successful" do
+  describe "GET 'home'" do # human description, can be anything
+    it "should be successful" do # what goes inside the quote marks is irrelevant
+      get 'home' # rspec acts like a browser and 'gets' home
+      response.should be_success # return a status of 200? boolean
+    end
+
+    it "check for page title" do
       get 'home'
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
       response.should be_success
+    end
+
+    it "check for page title" do
+      get 'about'
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
@@ -13,6 +33,12 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "check for page title" do
+      get 'contact'
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
