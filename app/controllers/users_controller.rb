a4b3b6d58e031d5d10377fda5834f24d7f0ba661
Robert Milner <dev@rbrtmlnr.com>
2012-02-06 16:57:06 -0600

    Finished cut of the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def new
     @title = "Sign up"
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    
+  end
 end
